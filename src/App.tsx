9c5be78ab822559502e5315c03f3599bddeea6ba
Maxim <mmandrrr@gmail.com>
2022-11-08 14:47:08 +0200

    removed map

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {HashRouter as Router,Routes,Route} from 'react-router-dom'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 
 import JobList from "./Components/JobList/JobList";
 import JobPage from './Components/JobPage/JobPage';
