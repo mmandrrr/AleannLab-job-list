9e78153a8f490cb5e162bb2290d83d3b67576221
Maxim <mmandrrr@gmail.com>
2022-11-11 20:19:13 +0200

    fixed key

--- src/services/GoogleAPI/GoogleAPI.ts
+++ src/services/GoogleAPI/GoogleAPI.ts
@@ -1,5 +1,5 @@
 class GoogleAPI {
-    _key = process.env.REACT_APP_GOOGLE_KEY;
+    _key : string = `${process.env.REACT_APP_GOOGLE_KEY}`;
 
     getLocation = async (url : string,key = this._key) => {
         const response = await fetch(`${url}${key}`)
